644850be4ac8bd0f7f9b7c843c72c5c05b86da79
Greg <80447308+GregBDesign@users.noreply.github.com>
2025-03-26 21:02:22 +1100

    Hardcode data into the load conversation dialog and log it out

--- loadconversationdialog.cpp
+++ loadconversationdialog.cpp
@@ -1,7 +1,7 @@
 #include "loadconversationdialog.h"
 
 LoadConversationDialog::LoadConversationDialog(QWidget *parent) :
-    QDialog{parent}, m_loadButton{this}, m_label{this}
+    QDialog{parent}, m_loadButton{this}, m_label{this}, m_tree{this}
 {
     setWindowTitle("Load conversation");
     setFixedSize(400, 200);
@@ -9,13 +9,42 @@ LoadConversationDialog::LoadConversationDialog(QWidget *parent) :
     m_label.move(50, 10);
     m_label.setText("Choose conversation to load:");
 
+    m_tree.setColumnCount(2);
+    m_tree.move(50, 30);
+    m_tree.setFixedHeight(110);
+    m_tree.setFixedWidth(300);
+    m_tree.setColumnWidth(0, 200);
+    m_tree.setColumnWidth(1, 95);
+    m_tree.setHeaderHidden(true);
+
     m_loadButton.move(50, 160);
     m_loadButton.setText("load");
     m_loadButton.setFixedSize(300, 30);
 
-    // next up - display tree widget
+    // hardcode the data for the minute
+    QList<QTreeWidgetItem*> items;
+
+    QTreeWidgetItem *item1 = new QTreeWidgetItem(&m_tree);
+
+    item1->setText(0, "Chairlift");
+    item1->setText(1, "24/03/25 19:49");
+
+    QTreeWidgetItem *item2 = new QTreeWidgetItem(&m_tree);
+
+    item2->setText(0, "Embedded software");
+    item2->setText(1, "20/02/25 11:11");
+
+    items.append(item1);
+    items.append(item2);
+
+    m_tree.insertTopLevelItems(0, items);
+
+    connect(&this->m_loadButton, &QPushButton::clicked, this, &LoadConversationDialog::onLoadClicked);
 }
 
-
-// do it with a tree widget - will eventually map the db types but for now let's
-// hardcode the data
+void LoadConversationDialog::onLoadClicked()
+{
+    emit conversationLoaded(m_tree.selectedItems().first()->text(0));
+    qDebug() << "conversation loaded is" << m_tree.selectedItems().first()->text(0);
+    close();
+}
--- loadconversationdialog.h
+++ loadconversationdialog.h
@@ -4,18 +4,21 @@
 #include <QDialog>
 #include <QPushButton>
 #include <QLabel>
+#include <QTreeWidget>
+#include <QTreeWidgetItem>
 
 class LoadConversationDialog : public QDialog
 {
     Q_OBJECT
     QPushButton m_loadButton;
     QLabel m_label;
+    QTreeWidget m_tree;
 public:
     LoadConversationDialog(QWidget *parent);
     void onLoadClicked();
 
 signals:
-    void conversationLoaded(); // todo - what's the arg type
+    void conversationLoaded(const QString selectedItem); // todo - what's the arg type
 };
 
 #endif // LOADCONVERSATIONDIALOG_H
